5c260a8f048d47809dea02675dce2d67be06b59d
Egor <predsedatel228@yandex.ru>
2024-01-20 16:52:47 +0300

    add solution for 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+function fileInfo(file) {
+  if (file.isFile()) {
+    let name = file.name.slice(0, file.name.lastIndexOf('.'));
+    let extension = file.name.slice(
+      file.name.lastIndexOf('.'),
+      file.name.length,
+    );
+    let filePath = path.join(folderPath, file.name);
+    fs.stat(filePath, (error, stat) => {
+      if (error) {
+        console.log(error.message);
+      }
+      stdout.write(`${name} - ${extension} - ${stat.size} bytes\n`);
+    });
+  }
+}
+
+fs.readdir(folderPath, { withFileTypes: true }, function (error, files) {
+  if (error) {
+    return console.log(error);
+  }
+  files.forEach((file) => {
+    fileInfo(file);
+  });
+  // console.log(files);
+});
